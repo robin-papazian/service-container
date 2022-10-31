cf859e8d50000394b809c06a4647835b86961b3d
robin-papazian <robin.papazian@laplateforme.io>
2022-10-31 13:14:32 +0100

    Developed a few tested

--- resources/Container.php
+++ resources/Container.php
@@ -2,6 +2,23 @@
 
 namespace Robin\Resources;
 
+
 class Container
 {
+    private array $services = [];
+
+    public function register(string $name, string $service): void
+    {
+        $this->services[$name] = $service;
+    }
+
+    public function registered(string $name): bool
+    {
+        return array_key_exists($name, $this->services);
+    }
+
+    public function getService(string $name)
+    {
+        return $this->services[$name];
+    }
 }
--- tests/Unit/Resources/ContainerTest.php
+++ tests/Unit/Resources/ContainerTest.php
@@ -2,6 +2,30 @@
 
 namespace Robin\Tests\Unit\Resources;
 
-class ContainerTest
+require_once __DIR__ . './../../../resources/Container.php';
+
+use Robin\Resources\Container;
+use PHPUnit\Framework\TestCase;
+
+
+class ContainerTest extends TestCase
 {
+    public function testInstantiable()
+    {
+        $this->assertNotNull(new Container);
+    }
+
+    public function testRegistered()
+    {
+        $container = new Container;
+        $container->register('foo', 'this is foo');
+        $this->assertTrue($container->registered('foo'));
+    }
+
+    public function testGet()
+    {
+        $container = new Container;
+        $container->register('foo', 'this is foo');
+        $this->assertEquals('this is foo', $container->getService('foo'));
+    }
 }
